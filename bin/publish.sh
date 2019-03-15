6fb8b664a554e0e967f927e9348c439de46b67dc
shun <shun@getto.systems>
2019-03-15 17:32:25 +0000

    add: gitlab-ci

---
+++ bin/publish.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+publish_main(){
+  npm install
+  : # npm run elm publish
+}
+
+publish_main
--- src/Getto/Command.elm
+++ src/Getto/Command.elm
@@ -4,12 +4,75 @@ module Getto.Command exposing
   , andThen
   )
 
+{-| utility of Cmd
+
+# Construction
+@docs none
+
+# Helper
+@docs map
+
+# Chaining
+@docs andThen
+ -}
+
+
+{-| create 'Do nothing' command
+
+    type alias Model =
+      { name : String
+      }
+
+    { name = "John" } |> Getto.Command.none
+ -}
 none : model -> ( model, Cmd msg )
 none model = ( model, Cmd.none )
 
+
+{-| map `Cmd msg` in command tuple
+
+    type alias Model = ()
+
+    type Msg
+      = Sub SubMsg
+
+    type SubMsg
+      = HelloWorld
+
+    update : Msg -> Model -> ( Model, Cmd Msg )
+    update message model =
+      case message of
+        Sub msg -> model |> updateSub msg |> Getto.Command.map Sub
+
+
+    updateSub : SubMsg -> Model -> ( Model, Cmd SubMsg )
+    updateSub message model =
+      case message of
+        HelloWorld -> ( model, Cmd.none )
+ -}
 map : (msg -> super) -> ( model, Cmd msg ) -> ( model, Cmd super )
 map = Cmd.map >> Tuple.mapSecond
 
+
+{-| merge 'Cmd msg' in command tuple
+
+    type alias Model = ()
+
+    type Msg
+      = HelloWorld
+
+    init : ( Model, Cmd Msg )
+    init =
+      ( (), Cmd.none )
+      |> Getto.Command.andThen initA
+      |> Getto.Command.andThen initB
+
+    initA : ( Model, Cmd Msg )
+    initA = ( (), Cmd.none )
+
+    initB : ( Model, Cmd Msg )
+    initB = ( (), Cmd.none )
+ -}
 andThen : (model -> ( model, Cmd msg )) -> ( model, Cmd msg ) -> ( model, Cmd msg )
 andThen f (model,cmd) =
   model |> f |> Tuple.mapSecond (\newCmd -> [cmd,newCmd] |> Cmd.batch)
